7be68268c975523173c16ba3abdcfe10e33fe556
kame <tentyo0620@icloud.com>
2022-08-23 16:22:27 +0900

    first commit

---
+++ nuxt/pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        {{ greet }}
+      </h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Context } from '@nuxt/types';
+
+export default Vue.extend({
+  async asyncData({ app }: Context) {
+    const greet: string = await app.$axios.$get('/').catch((err: unknown) => err)
+    return { greet }
+  }
+})
+</script>
